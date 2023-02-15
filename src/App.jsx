806d284b03c4ea74f46c7c52ac2576841cf72c6f
Robson Carvalho <robson73904@gmail.com>
2023-02-15 09:07:12 -0200

    :memo: Global Style component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+import { reset } from "./styles/global.style";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  html{
+    scroll-behavior: smooth;
+  }
+  body{
+    min-height: 100vh;
+    font-family: 'Inter', sans-serif;
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
+
 export const App = () => {
-  return;
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
 };
