d62975b927404cbfc48d81e204089a2f3edf4c37
Robson Carvalho <robson73904@gmail.com>
2023-02-16 17:48:09 -0200

    :memo: mobile button component updated

---
+++ src/components/MobileButton/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+
+export const MobileButton = () => {
+  return (
+    <Container
+      id="btn-mobile"
+      aria-label="Abrir menu"
+      aria-haspopup="true"
+      aria-controls="menu"
+      aria-expanded="false"
+    ></Container>
+  );
+};
