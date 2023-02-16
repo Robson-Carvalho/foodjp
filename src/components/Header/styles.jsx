1bb8c54313172481421bca9af32efc8e1727575d
Robson Carvalho <robson73904@gmail.com>
2023-02-16 18:23:36 -0200

    :memo: adding responsive in menu component

--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Container = styled.header`
   padding: 0 24px;
@@ -34,6 +34,33 @@ export const Menu = styled.ul`
   align-items: center;
   gap: 91px;
 
+  @media (max-width: 1120px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    background-color: ${({ theme }) => theme.white};
+
+    width: 100%;
+    height: 100vh;
+    padding-top: 64px;
+
+    position: fixed;
+    top: 47px;
+    left: 0;
+    transform: translateY(47px);
+
+    overflow-y: hidden;
+    visibility: hidden;
+    transition: 0.3s ease-in;
+
+    ${({ isMenuVisible }) =>
+      isMenuVisible &&
+      css`
+        height: 100%;
+        visibility: visible;
+      `}
+  }
+
   li {
     a {
       text-decoration: none;
@@ -57,6 +84,7 @@ export const Menu = styled.ul`
         border: 2px solid ${({ theme }) => theme.black};
         border-radius: 10px;
         transition: 0.3s;
+        margin: 0 auto;
 
         &:hover {
           color: ${({ theme }) => theme.black};
