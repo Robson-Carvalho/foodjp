f4c0f9591724205912d3eeb95439d21612e6e11b
Robson Carvalho <robson73904@gmail.com>
2023-02-17 09:37:54 -0200

    :memo: first section of additional main component

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,5 +1,18 @@
 import { Container } from "./styles";
 
 export const Main = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <section>
+        <div>
+          <h2>Comida Oriental</h2>
+          <span>
+            A culinária Japonesa é bastante equilibrada, sendo muito rica em
+            peixes (ômega 3), vegetais, massas e ingredientes frescos.
+          </span>
+          <a href="#">Cardápio</a>
+        </div>
+      </section>
+    </Container>
+  );
 };
